9be9ffbff120db8dc3034d92f8165cbea2e69d19
Ivan Krasilnikov <ivankrasilnikov2@gmail.com>
2018-07-25 15:02:35 +0500

    Законченный материал по express

--- express/app.js
+++ express/app.js
@@ -78,6 +78,7 @@ rout.route("/:id").get(function(request,response){
 app.use("/product", rout);
 */
 
+/*
 //Статические файлы
 //для вызыва компонента используется app.use(), express.static указывает, что компонент является статическим
 //__dirname + "/public") указывает на место, где располагается статический файл, а именно корневая папка + public (папка)
@@ -93,6 +94,94 @@ app.get("/", function(request, response){
 app.get("/contact", function(request, response){
     response.send("<h1>Contact</h1>");
 });
+*/
+
+/*
+//Работа с POST-запросами. 
+var bodyParser = require("body-parser");
+//bodyParser.urlencoded() - функция для парсинга данных с отправленной формы. Параметр {extended:false} задает то, как должен парситься объект
+//При использовании параметра true будет использоваться библиотека qs. Она позволяет парсить данные x-www-form-urlencoded. Так же она позволяет
+//создавать вложенные объекты, например { person: { name: 'bobby', age: '3' } }. Значение может являться массивом.
+//Параметр false вызывает библиотеку query-string, которая является облегчённым аналогом qs. Она не способна создавать вложенные объекты.
+//Данные представляются в виде ключ-значение { 'person[age]': '3', 'person[name]': 'bobby' }
+var urlencodeParser = bodyParser.urlencoded({extended:false});
+
+app.post("/register", urlencodeParser, function(request, response){
+    if (!request.body) response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
+*/
+
+/*
+//Работа с JSON и AJAX
+var bodyParser = require("body-parser");
+//Создаём оюъект для парсинга json
+var jsonParser = bodyParser.json();
+
+app.post("/user", jsonParser, function(request, response){
+    if(!request.body) return response.sendStatus(400);
+    //console.log(`${request.body.user} - ${request.body.age}`);
+    console.log(request.body);
+    //отправляем ответ обратно в формате json
+    response.json(`${request.body.userName} - ${request.body.userAge}`);
+});
+*/
+
+//Работа с движком представления hbs
+var hbs = require ("hbs");
+app.set("view engine", "hbs");
+
+//Для задействования частичных представлений необходимо использовать метод hbs.registerPartials()  
+//с указанием корневой папки + папки с представлениями
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.get("/", function(request, response){
+    //Указываем, что в качестве ответа отправляем файл index.hbs
+    response.render("index.hbs", {
+        num: ["1", "2", "3", "4"]
+    });
+});
+
+//Передача специальных данных. Указывается модель представления и параметры, которые передаются
+app.get("/contact", function(request, response){
+    response.render("contact.hbs", {
+        title: "Мои контакты",
+        email: "123",
+        phone: "123456",
+        mouseVisible : true,
+        mouse: ["razer", "a4tech"]
+    });
+});
+//Хелперы в hbs
+//hbs.registerHelper() говорит о том, что здесь мы задамём хелпер. Хелпер можно вставить в любую часть представления
+//но не только в виде строки, а ещё и в виде конкретного html кода. Первый параметр отвечает за название хелпера
+//второй отвечяает за функционал хелпера
+hbs.registerHelper("getTime", function(){
+    let myDate = new Date();
+    let hour = myDate.getHours();
+    let minute = myDate.getMinutes();
+    let second = myDate.getSeconds();
+    if(minute<10){
+        minute = "0" + minute;
+    }
+    if(second<10){
+        second = "0" + second;
+    }
+    return "Текущее время: " + hour + ":" + minute + ":" + second;
+});
+
+hbs.registerHelper("abc", function(array){
+    let result = "";
+    for(let i=0; i<array.length; i++){
+        result += "<li>" + array[i] + "</li>";
+    }
+    return new hbs.SafeString("<ul>" + result + "</ul>");
+});
+
+
+
+
 
 
 
